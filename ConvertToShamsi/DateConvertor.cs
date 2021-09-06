b1926436bbe7674c3fd6f897c2f39bd1b9343f03
Amin Asadi <aminasadiam@outlook.com>
2021-09-06 22:37:03 +0430

    Add files via upload

---
+++ ConvertToShamsi/DateConvertor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConvertToShamsi
+{
+    public static class DateConvertor
+    {
+        public static string ToShamsi(this DateTime value)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return pc.GetYear(value) + "/" + pc.GetMonth(value) + "/" +
+                pc.GetDayOfMonth(value).ToString("00");
+        }
+    }
+}
